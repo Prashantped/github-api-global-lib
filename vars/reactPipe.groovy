56eea39a65e71eb55848f20e5d2cd6d45f659054
Prashantped <108050027+Prashantped@users.noreply.github.com>
2023-05-19 09:44:17 +0530

    Create reactPipe.groovy

---
+++ vars/reactPipe.groovy
@@ -0,0 +1,44 @@
+def call(Map pipelineParams) {
+pipeline {
+    agent any
+environment {
+    IMAGE = 'https://hub.docker.com/_/${pipelineParams.node}'
+}
+stages {
+        stage('Install dependencies') {
+            steps {
+                sh 'npm -v'
+
+                echo 'Installing dependencies'
+                sh 'npm install'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Linting'
+                sh 'npm run lint'
+
+                echo 'Testing'
+                sh 'npm run test'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh "npm run build"
+            }
+        }
+        stage('Dockerize') {
+            parallel {
+                stage('latest') {                    
+                    steps {
+                        sh """
+                            docker build -t ${env.IMAGE} .
+                            docker push ${env.IMAGE}
+                        """
+                    }
+                }
+}
+        }
+}
+}
+}
